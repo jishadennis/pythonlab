a505b505a1c3dfe54de83388b54a01cb2d3088e3
jishadennis <20mp1577@rit.ac.in>
2021-05-06 21:46:46 +0530

    create labcycle2.2.2

---
+++ labcycle2.2.2.py
@@ -0,0 +1,7 @@
+S=input(“enter the list of words(in comma separated from)”)
+list1=s.split(“ ,“)
+Word=list1[0]
+For I in list 1:
+	If len(word)<len(i):
+		Word=i
+Print(“Length of the longest word:”,len(word))
